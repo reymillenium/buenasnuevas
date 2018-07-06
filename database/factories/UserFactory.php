8bf1c9b0f257880b281063acd97ccdeded5fc8cb
Reinier Garcia Ramos <reymillenium@gmail.com>
2018-07-06 04:42:40 -0400

    Added UserTableSeeder, UserFactory and modified migration of User table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,23 +1,41 @@
 <?php
-
-use Faker\Generator as Faker;
-
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(BuenasNuevas\User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-    ];
-});
+    
+    use Faker\Generator as Faker;
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Model Factories
+    |--------------------------------------------------------------------------
+    |
+    | This directory should contain each of the model factory definitions for
+    | your application. Factories provide a convenient way to generate new
+    | model instances for testing / seeding your application's database.
+    |
+    */
+    
+    $factory->define(BuenasNuevas\User::class, function (Faker $faker) {
+        
+        $kinds = ['administrator', 'operator', 'guest'];
+        $gender = $faker->randomElements(['male', 'female'])[0];
+        // $professions_amount = Profession::count();
+        
+        // dd($gender[0]);
+        
+        return [
+            // 'profession_id' => rand(1, $professions_amount),
+            'first_name' => $faker->firstName($gender),
+            // 'secondname' => $faker->name,
+            'last_name' => $faker->lastName,
+            
+            'email' => $faker->unique()->safeEmail,
+            'password' => bcrypt('123456'),
+            
+            'remember_token' => str_random(10),
+            'phone' => $faker->phoneNumber,
+            'gender' => $gender,
+            // 'is_active' => 1,
+            'kind' => $kinds[rand(0, sizeof($kinds) - 1)],
+            // 'website' => $faker->url,
+        ];
+        
+    });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+    
+    use BuenasNuevas\User;
+    use Illuminate\Database\Seeder;
+    
+    class UsersTableSeeder extends Seeder
+    {
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run()
+        {
+            // Obtenemos el user_profession_id directamente con Eloquent ORM
+            // $profession_id = Profession::where('name', 'Senior Back-end Developer')->value('id');
+            
+            // Usando Eloquent ORM. Muchísimo más sencillo aun!!! Usamos el Modelo User y llamamos a la
+            // función create, pasándole un array asociativo con los datos que queremos insertar
+            // Usuario especial # 1
+            User::create([
+                
+                // 'profession_id' => $profession_id,
+                
+                'first_name' => 'Reinier',
+                'second_name' => '',
+                'last_name' => 'Garcia Ramos',
+                
+                'email' => 'example1@gmail.com',
+                'password' => bcrypt('123456'),
+                
+                'phone' => '1111111111',
+                'gender' => 'male',
+                // 'is_active' => '1',
+                'kind' => 'administrator'
+            ]);
+            
+            // Creamos el usuario especial # 2
+            User::create([
+                
+                // 'profession_id' => 3,
+                
+                'first_name' => 'Pedro',
+                'second_name' => 'Picapiedras',
+                'last_name' => 'Roca Dura',
+                
+                'email' => 'pedrito@gmail.com',
+                'password' => bcrypt('123456'),
+                
+                'phone' => '2222222222',
+                'gender' => 'male',
+                // 'is_active' => '1',
+                'kind' => 'guest'
+            ]);
+            
+            // Obtenemos la cantidad de profesiones que existen en la BD con Eloquent ORM
+            // $professionsAmount = Profession::count();
+            
+            
+            // Creamos 100 usuarios aleatorios usando Model Factories
+            // factory(User::class)->times(100)->create();
+            factory(User::class, 100)->create();
+        }
+    }
